35f40f52d62d40ff8a79bf552693a06d98a0e181
skerdivelo <skerdivelo42@gmail.com>
2024-04-23 21:53:59 +0200

    chore: Update App.jsx with react-router-dom routing configuration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,58 @@
+import { createBrowserRouter, RouterProvider, Route, Outlet } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Write from "./pages/Write";
+import Home from "./pages/Home";
+import Single from "./pages/Single";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const Layout = () => {
+  return (
+      <>
+          <Navbar/>
+          <Outlet/>
+          <Footer/>
+      </>
+  );
+};
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout/>,
+        children: [
+            {
+                path: "/",
+                element: <Home/>
+            },
+            {
+                path: "/post/:id",
+                element: <Single/>
+            },
+            {
+                path: "/write",
+                element: <Write/>
+            },
+        ]
+    },
+    {
+        path: "/register",
+        element: <Register/>
+    },
+    {
+        path: "/login",
+        element: <Login/>
+    },
+]);
+
 function App() {
-  return <div>Hello World!</div>;
+    return (
+      <div>
+        <RouterProvider router={router}/>
+      </div>
+    );
 }
 
 export default App;
